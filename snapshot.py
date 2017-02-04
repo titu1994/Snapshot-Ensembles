07996e5b8a0820dd99f2d3ef050b8c2e317460fd
Somshubra Majumdar <titu1994@gmail.com>
2017-02-04 11:27:59 -0600

    Added documentation to snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -5,28 +5,65 @@ import keras.callbacks as callbacks
 from keras.callbacks import Callback
 
 class SnapshotModelCheckpoint(Callback):
+    """Callback that saves the snapshot weights of the model.
 
-    def __init__(self, T, M, fn_prefix='Model'):
+    Saves the model weights on certain epochs (which can be considered the
+    snapshot of the model at that epoch).
+
+    Should be used with the cosine annealing learning rate schedule to save
+    the weight just before learning rate is sharply increased.
+
+    # Arguments:
+        nb_epochs: total number of epochs that the model will be trained for.
+        nb_snapshots: number of times the weights of the model will be saved.
+        fn_prefix: prefix for the filename of the weights.
+    """
+
+    def __init__(self, nb_epochs, nb_snapshots, fn_prefix='Model'):
         super(SnapshotModelCheckpoint, self).__init__()
 
-        self.check = T // M
+        self.check = nb_epochs // nb_snapshots
         self.fn_prefix = fn_prefix
 
     def on_epoch_end(self, epoch, logs={}):
         if epoch != 0 and (epoch + 1) % self.check == 0:
             filepath = self.fn_prefix + "-%d.h5" % ((epoch + 1) // self.check)
             self.model.save_weights(filepath, overwrite=True)
-            print("Saved snapshot at weights/%s_%d.h5" % (self.fn_prefix, epoch))
+            #print("Saved snapshot at weights/%s_%d.h5" % (self.fn_prefix, epoch))
 
 
 class SnapshotCallbackBuilder:
+    """Callback builder for snapshot ensemble training of a model.
 
-    def __init__(self, T, M, alpha_zero=0.1):
-        self.T = T
-        self.M = M
-        self.alpha_zero = alpha_zero
+    Creates a list of callbacks, which are provided when training a model
+    so as to save the model weights at certain epochs, and then sharply
+    increase the learning rate.
+    """
+
+    def __init__(self, nb_epochs, nb_snapshots, init_lr=0.1):
+        """
+        Initialize a snapshot callback builder.
+
+        # Arguments:
+            nb_epochs: total number of epochs that the model will be trained for.
+            nb_snapshots: number of times the weights of the model will be saved.
+            init_lr: initial learning rate
+        """
+        self.T = nb_epochs
+        self.M = nb_snapshots
+        self.alpha_zero = init_lr
 
     def get_callbacks(self, model_prefix='Model'):
+        """
+        Creates a list of callbacks that can be used during training to create a
+        snapshot ensemble of the model.
+
+        Args:
+            model_prefix: prefix for the filename of the weights.
+
+        Returns: list of 3 callbacks [ModelCheckpoint, LearningRateScheduler,
+                 SnapshotModelCheckpoint] which can be provided to the 'fit' function
+        """
         if not os.path.exists('weights/'):
             os.makedirs('weights/')
 
